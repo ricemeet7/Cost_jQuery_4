b84604e1f86e3ab0acb03c7cbfb22ac9c408e2fd
ricemeet7 <110725120+ricemeet7@users.noreply.github.com>
2022-11-24 05:59:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+jQuery(".slick").slick({
+  dots:true,
+  arrows: false, // 矢印
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  slidesToShow: 1,
+  centerMode: true,
+  centerPadding: "18%",
+  variableWidth: true,
+  slidesToScroll: 1
+ });
